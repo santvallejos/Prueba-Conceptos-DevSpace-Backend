25336d24bf1e2fb8e9b532674a39de3d07cdab08
Santiago Vallejos <vallejossantiago1412@gmail.com>
2025-03-17 20:11:52 -0300

    MongoDB Crud Operation with .Net

--- DevSpace-WebAPI/obj/Debug/net9.0/DevSpace-WebAPI.AssemblyInfo.cs
+++ DevSpace-WebAPI/obj/Debug/net9.0/DevSpace-WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevSpace-WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6e382bc4af207146c997023eff3bb8d700736a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9c2d0dd0f7a7be5e3451d06231862d431f8527")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevSpace-WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevSpace-WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
